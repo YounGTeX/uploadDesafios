7597e9b7f674991da7ca4d522c4eb8befb333ee5
Thiago Pankievicz <thiagoextremer@gmail.com>
2021-06-08 19:44:16 -0300

    adding files

--- Desafio7/desafio7/karma.conf.js
+++ Desafio7/desafio7/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/desafio7'),
+      dir: require('path').join(__dirname, './coverage/Desafio7'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- Desafio7/desafio7/src/app/app-routing.module.ts
+++ Desafio7/desafio7/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'categories', loadChildren: './pages/categories/categories.module' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
